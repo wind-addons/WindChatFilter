f7126727969241681416df5534ee1250d7358f8e
Zhou Fang <fang2hou@outlook.com>
2023-02-11 21:55:40 +0900

    settings: remove old version db

--- Settings/Core.lua
+++ Settings/Core.lua
@@ -3,7 +3,6 @@ local W, F, L = unpack(ns)
 local ADB = LibStub("AceDB-3.0")
 
 ns[4] = {
-    enable = true,
     minimapIcon = {
         hide = false
     }
@@ -12,9 +11,7 @@ ns[4] = {
 ns[5] = {
     advanced = {
         logLevel = 2,
-        loginMessage = true
     },
-    guidCache = {}
 }
 
 function W:BuildDatabase()
@@ -31,4 +28,8 @@ function W:BuildDatabase()
     self.Database.RegisterCallback(self, "OnProfileChanged")
     self.db = self.Database.profile
     self.global = self.Database.global
+
+    if self.global.guidCache then -- Migrate from oldversion
+        self.global.guidCache = nil
+    end
 end
