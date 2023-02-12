f83e41981517f607ecffbb0735e1eb435e02be08
Zhou Fang <fang2hou@outlook.com>
2023-02-12 15:20:09 +0900

    feat: add group invite module

---
+++ Modules/GroupInvite.lua
@@ -0,0 +1,96 @@
+local W, F, L = unpack(select(2, ...))
+local GI = W:NewModule("GroupInvite", "AceEvent-3.0")
+
+local smartModeNames = {
+    ["提升"] = true,
+    ["代充"] = true,
+    ["清水"] = true,
+    ["团本"] = true,
+    ["團本"] = true,
+    ["团木"] = true,
+    ["團木"] = true,
+    ["團長"] = true,
+    ["团长"] = true,
+    ["秘境"] = true,
+    ["大秘"] = true,
+    ["大米"] = true,
+    ["大密"] = true,
+    ["秘境"] = true,
+    ["密境"] = true,
+    ["米境"] = true,
+    ["消费"] = true,
+    ["消費"] = true,
+    ["专车"] = true,
+    ["專車"] = true,
+    ["找我"] = true
+}
+
+function GI:RequestHandler(_, name, _, _, _, _, _, guid)
+    if not self.db.enabled then
+        return
+    end
+
+    if not guid or guid == "" or guid == W.myGUID then
+        return
+    end
+
+    local isGuildMember = IsGuildMember(guid)
+    local isFriend = C_FriendList.IsFriend(guid)
+    local isBNFriend = C_BattleNet.GetAccountInfoByGUID(guid) ~= nil
+
+    local playerInfo = F.FetchPlayerInfo(guid)
+
+    if self.db.onlyFriendsOrGuildMembers then
+        if not (isGuildMember or isFriend or isBNFriend) then
+            self:Log("debug", "Decline invite from player: " .. name)
+            StaticPopup_Hide("PARTY_INVITE")
+        end
+    end
+
+    if self.db.smartMode then
+        if not (isGuildMember or isFriend or isBNFriend) then
+            local playerInfo = F.FetchPlayerInfo(guid)
+
+            if playerInfo then
+                if playerInfo.race == "Pandaren" and playerInfo.class == "DEATHKNIGHT" then
+                    self:Log("debug", "Decline invite from pandaren player: " .. playerInfo.name)
+                    StaticPopup_Hide("PARTY_INVITE")
+                else
+                    local matched
+                    for name, _ in pairs(smartModeNames) do
+                        if strfind(playerInfo.name, name) then
+                            matched = true
+                            break
+                        end
+                    end
+                    if not matched then
+                        self:Log("debug", "Decline invite from player: " .. playerInfo.name)
+                        StaticPopup_Hide("PARTY_INVITE")
+                    end
+                end
+            end
+        end
+    end
+end
+
+function GI:OnInitialize()
+    self.db = W.db.groupInvite
+
+    if not self.db.enabled then
+        return
+    end
+
+    self:RegisterEvent("PARTY_INVITE_REQUEST", "RequestHandler")
+
+    self.initialized = true
+end
+
+function GI:ProfileUpdate()
+    self.db = W.db.groupInvite
+
+    if self.db.enabled then
+        if not self.initialized then
+            self:OnInitialize()
+        end
+    end
+end
