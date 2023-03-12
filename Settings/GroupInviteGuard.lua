099f97dc7ab5cd6e4d723e05a2b34f6ddd7b90dc
Zhou Fang <fang2hou@outlook.com>
2023-03-12 11:32:35 +0900

    setting(gig): add allow whispered target

--- Settings/GroupInviteGuard.lua
+++ Settings/GroupInviteGuard.lua
@@ -3,6 +3,7 @@ local W, F, L, P = unpack(select(2, ...))
 P.groupInviteGuard = {
     enabled = true,
     displayMessageAfterRejecting = true,
+    allowWhisperedTarget = true,
     smartMode = true,
     onlyFriendsOrGuildMembers = false,
     chatFilterMode = false
