5474ea5e303c6628b91bd3fded3cb379b992a880
Zhou Fang <fang2hou@outlook.com>
2023-03-12 11:28:47 +0900

    setting: add new mode default setting

--- Settings/GroupInviteGuard.lua
+++ Settings/GroupInviteGuard.lua
@@ -2,7 +2,8 @@ local W, F, L, P = unpack(select(2, ...))
 
 P.groupInviteGuard = {
     enabled = true,
-    onlyFriendsOrGuildMembers = false,
-    smartMode = true,
     displayMessageAfterRejecting = true,
+    smartMode = true,
+    onlyFriendsOrGuildMembers = false,
+    chatFilterMode = false
 }
