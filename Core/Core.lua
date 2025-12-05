5cca3afdafbd3f89bc3898cb92e4074e65fa4f57
Zhou Fang <fang2hou@outlook.com>
2025-12-05 22:42:41 +0900

    chore: cleanup the prefix register

--- Core/Core.lua
+++ Core/Core.lua
@@ -1,13 +1,10 @@
 local W, F, L = unpack(select(2, ...))
 
 local LibStub = _G.LibStub
-local C_ChatInfo_RegisterAddonMessagePrefix = C_ChatInfo.RegisterAddonMessagePrefix
 
 function W:OnInitialize()
 	self:BuildDatabase()
 	self:BuildOptions()
-
-	C_ChatInfo_RegisterAddonMessagePrefix(self.AddonMsgPrefix)
 end
 
 function W:OnProfileChanged()
